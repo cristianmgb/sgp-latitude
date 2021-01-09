31f22f3b9aa190078f0df06a35cad82328a00554
Jorge <jarmandopacheco@gmail.com>
2021-01-08 20:14:28 -0500

    Se crean el controlador y las vistas para los 'Usuarios'

---
+++ app/Controllers/User.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controllers;
+
+class User extends BaseController
+{
+	
+	public function index()
+	{
+		return view('user/all');
+	}
+
+	/**
+	 * [new description]
+	 * @return [type] [description]
+	 */
+	public function new()
+	{
+		return view('user/new');
+	}
+
+	/**
+	 * [create description]
+	 * @return [type] [description]
+	 */
+	public function create()
+	{
+
+	}
+
+	/**
+	 * [show description]
+	 * @param  [type] $id [description]
+	 * @return [type]     [description]
+	 */
+	public function show( $id )
+	{
+
+	}
+
+	/**
+	 * [edit description]
+	 * @param  [type] $id [description]
+	 * @return [type]     [description]
+	 */
+	public function edit( $id )
+	{
+
+	}
+
+	/**
+	 * [update description]
+	 * @param  [type] $id [description]
+	 * @return [type]     [description]
+	 */
+	public function update( $id )
+	{
+
+	}
+
+	/**
+	 * [remove description]
+	 * @param  [type] $id [description]
+	 * @return [type]     [description]
+	 */
+	public function remove( $id )
+	{
+
+	/**
+	 * [delete description]
+	 * @param  [type] $id [description]
+	 * @return [type]     [description]
+	 */
+	}public function delete( $id )
+	{
+
+	}
+}
+
+/* End of file User.php */
+/* Location: ..app/Controllers/User.php */
---
+++ app/Views/user/all.php
@@ -0,0 +1,59 @@
+<?= $this->extend('./layouts/dashboard') ?>
+
+<?= $this->section('title') ?>
+	Usuarios
+<?= $this->endSection() ?>
+
+<?= $this->section('breadcrumb') ?>
+	<div class="c-subheader">
+		<ol class="breadcrumb border-0 m-0">
+			<li class="breadcrumb-item">Inicio</li>
+			<li class="breadcrumb-item"><?= anchor('admin/usuarios', 'Usuarios') ?></li>
+		</ol>
+	</div>
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+	<div class="row">
+		<div class="col">
+			<div class="card">
+				<div class="card-header">
+					<i class="fa fa-align-justify"></i> Usuarios
+				</div>
+				<div class="card-body p-0">
+					<table class="table table-responsive-sm table-striped table-sm">
+						<thead>
+							<tr class="text-center">
+								<th>Nombre</th>
+								<th>Descripción</th>
+								<th>Creado</th>
+								<th>Modificado</th>
+								<th width="100px">Estado</th>
+								<th width="100px">Acción</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Superadmin</td>
+								<td>xxxxxxxxxxxxxxxx</td>
+								<td><span class="badge badge-secondary">01/09/2021</span></td>
+								<td><span class="badge badge-info">01/09/2021</span></td>
+								<td><span class="badge badge-success">Active</span></td>
+								<td>
+									<div class="btn-group btn-group-sm" role="group" aria-label="tools">
+										<button class="btn btn-outline-info" type="button">
+											<i class="far fa-edit"></i>
+										</button>
+										<button class="btn btn-outline-danger" type="button">
+											<i class="far fa-trash-alt"></i>
+										</button>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+<?= $this->endSection() ?>

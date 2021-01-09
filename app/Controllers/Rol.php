6e3d391e60bcb783b79e3b6b178b624ba28ebf89
Jorge <jarmandopacheco@gmail.com>
2021-01-08 20:14:04 -0500

    Se crea el controlador y a vista para el 'Rol'

---
+++ app/Controllers/Rol.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+class Rol extends BaseController
+{
+	public function index()
+	{
+		return view('rol');
+	}
+
+	// Add a new item
+	public function add()
+	{
+
+	}
+
+	//Update one item
+	public function update( $id = NULL )
+	{
+
+	}
+
+	//Delete one item
+	public function delete( $id = NULL )
+	{
+
+	}
+}
+
+/* End of file Rol.php */
+/* Location: ..app/Controllers/Rol.php */

8ba13aba7d223ade6c4e33c53574d4cf7663e80e
Jorge <jarmandopacheco@gmail.com>
2021-01-08 20:15:51 -0500

    Se crea el controlador y vista para los 'Impuestos'

---
+++ app/Controllers/Tax.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+class Tax extends BaseController
+{
+	public function index()
+	{
+		return view('tax');
+	}
+
+	// Add a new item
+	public function add()
+	{
+
+	}
+
+	//Update one item
+	public function update( $id = NULL )
+	{
+
+	}
+
+	//Delete one item
+	public function delete( $id = NULL )
+	{
+
+	}
+}
+
+/* End of file Tax.php */
+/* Location: ..app/Controllers/Tax.php */

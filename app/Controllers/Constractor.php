3aa030d87b1b003ac58135d5569cef45edf4b6f5
Jorge <jarmandopacheco@gmail.com>
2021-01-08 20:12:41 -0500

    Se crea el controlador y vista para los 'Contratistas'

---
+++ app/Controllers/Constractor.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+class Constractor extends BaseController
+{
+	public function index()
+	{
+		return view('constractor');
+	}
+
+	// Add a new item
+	public function add()
+	{
+
+	}
+
+	//Update one item
+	public function update( $id = NULL )
+	{
+
+	}
+
+	//Delete one item
+	public function delete( $id = NULL )
+	{
+
+	}
+}
+
+/* End of file Constractor.php */
+/* Location: ..app/Controllers/Constractor.php */

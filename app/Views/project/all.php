fa7bd22a0ef14da090c5b1a7ec016e94c5a07271
Jorge armando <jarmandopacheco@gmail.com>
2021-01-17 22:59:19 -0500

    Se crean las vistas para el modulo de 'Proyectos'

--- app/Views/project/all.php
+++ app/Views/project/all.php
@@ -14,6 +14,29 @@
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-
-
+	<div class="row">
+		<div class="col">
+			<div class="card card-accent-primary">
+				<div class="card-header">
+					<i class="fa fa-align-justify"></i> Proyectos
+				</div>
+				<div class="card-body p-0">
+					<table class="table table-responsive-sm table-striped table-sm">
+						<thead>
+							<tr class="text-center">
+								<th>Nombre</th>
+								<th>Valor</th>
+								<th>Tiempo</th>
+								<th>Creado</th>
+								<th>Modificado</th>
+								<th width="100px">Estado</th>
+								<th width="100px">Acción</th>
+							</tr>
+						</thead>
+						<tbody id="projects"></tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
 <?= $this->endSection() ?>
--- app/Views/project/new.php
+++ app/Views/project/new.php
@@ -15,6 +15,60 @@
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-
-
+	<div class="row">
+		<div class="col">
+			<div class="card card-accent-primary">
+				<div class="card-header">
+					<i class="fa fa-user-plus"></i> Nuevo Proyecto
+				</div>
+				<div class="card-body">
+					<form>
+						<div class="row">
+							<div class="col">
+								<div class="form-group">
+									<label for="name">Nombre</label>
+									<input type="text" class="form-control" id="name" name="name">
+								</div>
+							</div>
+							<div class="col">
+								<div class="form-group">
+									<label for="name">Nombre</label>
+									<input type="text" class="form-control" id="name" name="name">
+								</div>
+							</div>
+							<div class="col">
+								<div class="form-group">
+									<label for="name">Nombre</label>
+									<input type="text" class="form-control" id="name" name="name">
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col">
+								<hr>
+							</div>
+						</div>
+						<div class="row justify-content-center">
+							<div class="col-8">
+								<div class="form-group">
+						      		<div class="row">
+										<div class="col">
+								        	<button type="button" class="btn btn-block btn-secondary">
+								        		<i class="far fa-times-circle"></i> Cancelar
+								        	</button>
+								        </div>
+								        <div class="col">
+								        	<button type="button" class="btn btn-block btn-primary">
+								        		<i class="far fa-check-circle"></i> Guardar
+								        	</button>
+								        </div>
+								    </div>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 <?= $this->endSection() ?>

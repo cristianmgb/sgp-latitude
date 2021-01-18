efce00019e7143711eddee20c5ceedf0b64773b5
Jorge armando <jarmandopacheco@gmail.com>
2021-01-17 22:59:40 -0500

    Se crea la vista 'Detalle del rol'

---
+++ app/Views/rolDetail.php
@@ -0,0 +1,18 @@
+<?= $this->extend('layouts/dashboard') ?>
+
+<?= $this->section('title') ?>
+	Roles
+<?= $this->endSection() ?>
+
+<?= $this->section('breadcrumb') ?>
+	<div class="c-subheader">
+		<ol class="breadcrumb border-0 m-0">
+			<li class="breadcrumb-item">Inicio</li>
+			<li class="breadcrumb-item"><?= anchor('admin/roles', 'Roles') ?></li>
+		</ol>
+	</div>
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+	<?php echo $rol ?>
+<?= $this->endSection() ?>

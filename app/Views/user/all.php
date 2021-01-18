03b868514d60210586aa1e3592e01fe4b213ac0a
Jorge armando <jarmandopacheco@gmail.com>
2021-01-17 22:56:57 -0500

    Se crean las vistas y componentes apra los 'Usuarios'

--- app/Views/user/all.php
+++ app/Views/user/all.php
@@ -14,46 +14,5 @@
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-	<div class="row">
-		<div class="col">
-			<div class="card">
-				<div class="card-header">
-					<i class="fa fa-align-justify"></i> Usuarios
-				</div>
-				<div class="card-body p-0">
-					<table class="table table-responsive-sm table-striped table-sm">
-						<thead>
-							<tr class="text-center">
-								<th>Nombre</th>
-								<th>Descripción</th>
-								<th>Creado</th>
-								<th>Modificado</th>
-								<th width="100px">Estado</th>
-								<th width="100px">Acción</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>Superadmin</td>
-								<td>xxxxxxxxxxxxxxxx</td>
-								<td><span class="badge badge-secondary">01/09/2021</span></td>
-								<td><span class="badge badge-info">01/09/2021</span></td>
-								<td><span class="badge badge-success">Active</span></td>
-								<td>
-									<div class="btn-group btn-group-sm" role="group" aria-label="tools">
-										<button class="btn btn-outline-info" type="button">
-											<i class="far fa-edit"></i>
-										</button>
-										<button class="btn btn-outline-danger" type="button">
-											<i class="far fa-trash-alt"></i>
-										</button>
-									</div>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-	</div>
+	<sgp-user-table></sgp-user-table>
 <?= $this->endSection() ?>
---
+++ src/js/components/UserTableComponent.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="row">
+		<div class="col">
+			<div class="card card-accent-primary">
+				<div class="card-header">
+					<i class="fa fa-align-justify"></i> Usuarios
+				</div>
+				<div class="card-body p-0">
+					<table class="table table-responsive-sm table-striped table-sm">
+						<thead>
+							<tr class="text-center">
+								<th>Nombres</th>
+								<th>Apellidos</th>
+								<th>Rol</th>
+								<th>Teléfono</th>
+								<th>Creado</th>
+								<th>Modificado</th>
+								<th width="100px">Estado</th>
+								<th width="100px">Acción</th>
+							</tr>
+						</thead>
+						<tbody id="users"></tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name : 'user-table',
+		data: () => ({
+			
+		}),
+		methods: {
+			//
+		}
+	};
+</script>

f85473fc24edd5790905dcb2d03cef4483f78989
Jorge armando <jarmandopacheco@gmail.com>
2021-02-01 00:10:32 -0500

    Se remueve el aparte de 'Potenciado por CoreUI' del footer

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -2,5 +2,4 @@
     <div>
         <i class="fas fa-copyright" aria-hidden="true"></i> Todos los derechos reservados <a href="https://lattitude.co" target="_blank"><strong>Lattitude</strong></a> - 2021
     </div>
-    <div class="ml-auto">Potenciado por <a href="https://coreui.io/" target="_blank">CoreUI</a></div>
 </footer>

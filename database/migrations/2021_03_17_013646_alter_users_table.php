93b0a92465b770bf3197aa18a891e3397d4e398c
Jorge armando <jarmandopacheco@gmail.com>
2021-03-18 23:59:58 -0500

    Se crea la migración para modificar la tabla de usuarios

---
+++ database/migrations/2021_03_17_013646_alter_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('associate_id')
+                ->unsigned()
+                ->index()
+                ->nullable()
+                ->after('status');
+            $table->foreign('associate_id')
+                ->references('id')
+                ->on('associates')
+                ->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
